afb9aa8160dad19137d688d3b8f0b4361dc1db74
Istabraq Kulow <istakulow@gmail.com>
2024-07-10 13:04:55 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
+  
+  function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+      console.log(i);
+    }
+  }
